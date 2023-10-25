0d94fd3d69ac3e4fac02b85f90e01cb2707a9d0f
Gazer29 <gazer29@hotmail.com>
2023-10-25 17:13:40 +0100

    Minor changes

--- ButtonConfig.lua
+++ ButtonConfig.lua
@@ -8,34 +8,33 @@ local ButtonConfig = {
     {label = "Settings",id = "settignsButton",x = 5,y = 5,width = 10,height = 3,type = "push",enabled = true,page = "main",action = function()    goToPage("settings") end},
     {label = "Map",id = "mapButton",x = 5,y = 5,width = 10,height = 3,type = "push",enabled = true,page = "main",action = function()    goToPage("Map") end},
     {label = "Back",id = "backButton",x = 5,y = 5,width = 10,height = 3,type = "push",enabled = true,page = "settings",action = function()    goToPage("main") end},
-
+--resources
     {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("main") end,enabled = true, type = "button",page = "resources"},
     {id = "Refresh",width = 3,x = -5,y = 0,colorOn = colors.blue,colorOff = colors.gray,charOn = "A",action = function() RefreshFlag() end,enabled = true, type = "button",page = "resources"},
     {id = "addBtn",justify = "right",width = 3,colorOn = colors.green,colorOff = colors.gray,charOn = "+",action = function() print("Add button pressed!") end,enabled = false, type= "list",page = "resources"},
     {id = "removeBtn",justify = "right",width = 3,colorOn = colors.red,colorOff = colors.gray,charOn = "-",action = function() print("Remove button pressed!") end,enabled = false, type = "list",page = "resources"},
     {id = "toggle",justify = "left",width = 3,colorOn = colors.yellow,colorOff = colors.blue,charOn = "O",action = function(x) handleItem(x) end,enabled = false, type = "list",page = "resources"},
-
+--upgrades
     {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("main") end,enabled = true, type = "button",page = "upgrades"},
     {id = "toggle",justify = "left",width = 3,colorOn = colors.yellow,colorOff = colors.blue,charOn = "O",enabled = false, type = "list",page = "upgrades"},
     {id = "addBtn",justify = "right",width = 3,colorOn = colors.green,colorOff = colors.gray,charOn = "+",action = function(x) goToDisplayPage(x,"display_upgrade") end,enabled = true, type= "list",page = "upgrades"},
-  
+--display upgrades
     {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("upgrades") end,enabled = true, type = "button",page = "display_upgrade"},
     {id = "Up",width = 3,x = -1,y = 1,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function(x) UpgradeSchedule(x) goToPage("upgrades") if x.enabled then adjustItems(x) end end,enabled = false, type = "button",page = "display_upgrade"},
     {id = "toggle",justify = "right",width = 3,colorOn = colors.green,colorOff = colors.gray,charOn = "+",enabled = true, type= "list",page = "display_upgrade"},
-
+--production
     {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("main") end,enabled = true, type = "button",page = "production"},
     {id = "toggle",justify = "left",width = 3,colorOn = colors.yellow,colorOff = colors.blue,charOn = "O",enabled = false, type = "list",page = "production"},
     {id = "addBtn",justify = "right",width = 3,colorOn = colors.green,colorOff = colors.gray,charOn = "+",action = function(x) goToDisplayPage(x,"display_production") end,enabled = true, type= "list",page = "production"},
-
+--display production
     {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("production") end,enabled = true, type = "button",page = "display_production"},
     {id = "Up",width = 3,x = -1,y = 1,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function(x) handleProduction(x) goToPage("production") end,enabled = false, type = "button",page = "display_production"},
     {id = "toggle",justify = "right",width = 3,colorOn = colors.green,colorOff = colors.gray,charOn = "+",enabled = true, type= "list",page = "display_production"},
 --Map
     {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("main") end,enabled = true, type = "button",page = "Map"},
-    {id = "Add",type = "button",page = "Map",width = 3,x = -4,y = 0,action = function() OffsetZoom(1) end,charOn = "+",colorOn = colors.yellow,colorOff = colors.gray,enabled = true},
-    {id = "Subtract",type = "button",page = "Map",width = 3,x = -7,y = 0,action = function() OffsetZoom(-1) end,charOn = "-",colorOn = colors.yellow,colorOff = colors.gray,enabled = true},
-    
-    --Settings
+    {id = "Add",type = "button",page = "Map",width = 3,x = -4,y = 0,action = function() OffsetZoom(-1) end,charOn = "+",colorOn = colors.yellow,colorOff = colors.gray,enabled = true},
+    {id = "Subtract",type = "button",page = "Map",width = 3,x = -7,y = 0,action = function() OffsetZoom(1) end,charOn = "-",colorOn = colors.yellow,colorOff = colors.gray,enabled = true},
+--Settings
 {label = "InputChest",id = "settigns_inputchestButton",x = 5,y = 5,width = 10,height = 3,type = "push",enabled = true,page = "settings",action = function()    goToPage("settings_InputChest") end},
 {label = "OutputChest",id = "settigns_outputchestButton",x = 5,y = 5,width = 10,height = 3,type = "push",enabled = true,page = "settings",action = function()    goToPage("settings_OutputChest") end},
 {label = "InputPOP",id = "settigns_inputPOPButton",x = 5,y = 5,width = 10,height = 3,type = "push",enabled = true,page = "settings",action = function()    InputPOP() end},
@@ -43,14 +42,14 @@ local ButtonConfig = {
 
 {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("settings") end,enabled = true, type = "button",page = "settings_InputChest"},
 {id = "Back",width = 3,x = -1,y = 0,colorOn = colors.yellow,colorOff = colors.gray,charOn = "B",action = function() goToPage("settings") end,enabled = true, type = "button",page = "settings_OutputChest"},
-
+--InputChest
 {id = "Add",width = 3,x = 2,y = 2,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function() ChangeInputChest(1,0,0) end,enabled = true, type = "button",page = "settings_InputChest"},
 {id = "Subtract",width = 3,x = 2,y = 6,colorOn = colors.yellow,colorOff = colors.gray,charOn = "-",action = function() ChangeInputChest(-1,0,0) end,enabled = true, type = "button",page = "settings_InputChest"},
 {id = "Add",width = 3,x = 8,y = 2,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function() ChangeInputChest(0,1,0) end,enabled = true, type = "button",page = "settings_InputChest"},
 {id = "Subtract",width = 3,x = 8,y = 6,colorOn = colors.yellow,colorOff = colors.gray,charOn = "-",action = function() ChangeInputChest(0,-1,0) end,enabled = true, type = "button",page = "settings_InputChest"},
 {id = "Add",width = 3,x = 14,y = 2,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function() ChangeInputChest(0,0,1) end,enabled = true, type = "button",page = "settings_InputChest"},
 {id = "Subtract",width = 3,x = 14,y = 6,colorOn = colors.yellow,colorOff = colors.gray,charOn = "-",action = function() ChangeInputChest(0,0,-1) end,enabled = true, type = "button",page = "settings_InputChest"},
-
+--OutputChest
 {id = "Add",width = 3,x = 2,y = 2,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function() ChangeOutputChest(1,0,0) end,enabled = true, type = "button",page = "settings_OutputChest"},
 {id = "Subtract",width = 3,x = 2,y = 6,colorOn = colors.yellow,colorOff = colors.gray,charOn = "-",action = function() ChangeOutputChest(-1,0,0) end,enabled = true, type = "button",page = "settings_OutputChest"},
 {id = "Add",width = 3,x = 8,y = 2,colorOn = colors.yellow,colorOff = colors.gray,charOn = "+",action = function() ChangeOutputChest(0,1,0) end,enabled = true, type = "button",page = "settings_OutputChest"},
