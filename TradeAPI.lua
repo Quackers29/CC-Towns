8db127cc370545b9ff20bf4627b09af442b1cb44
Gazer29 <gazer29@hotmail.com>
2023-11-14 17:27:24 +0000

    Update TradeAPI.lua

--- TradeAPI.lua
+++ TradeAPI.lua
@@ -323,7 +323,7 @@ function TradeAPI.SellerUpdateOffers(tradeFile,SettingsFile,resFile)
                 if trades.offers.selling[i] ~= nil then
                     continue = false
                 end
-                if trades.offers.accepted[i] ~= nil then
+                if trades.offers.accepted and trades.offers.accepted[i] ~= nil then
                     continue = false
                 end
                 if continue then -- keepInstock item not in sell list, check resources
