d4a92d5cf89b5260a331d455a3542963a7ecf772
Gazer29 <gazer29@hotmail.com>
2023-11-30 18:01:37 +0000

    Create Stats.lua

---
+++ Stats.lua
@@ -0,0 +1,8 @@
+local Utility = require("Utility")
+--local adminFile = "AdminSettings.json"
+
+while true do
+    local Alltowns = Utility.FindAllTowns()
+    commands.scoreboard.players.set("#Towns", "AllTowns", #Alltowns)
+    os.sleep(30)
+end
