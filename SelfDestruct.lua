0df9a57c42b0818e47701f6187d6ee00fda97dab
Gazer29 <gazer29@hotmail.com>
2023-12-01 14:05:19 +0000

    Update SelfDestruct.lua

--- SelfDestruct.lua
+++ SelfDestruct.lua
@@ -1,4 +1,17 @@
 local Utility = require("Utility")
+local townNames = "Defaults\\townNames.json"
+local x,y,z = gps.locate()
+local townFolder = "Town_X"..x.."Y"..y.."Z"..z
+local town = "Towns\\"..townFolder.."\\"
+local SettingsFile = town.."SET_X"..x.."Y"..y.."Z"..z..".json"
+
+
 print("Computer will Self Destruct in 5 seconds")
 os.sleep(5)
+local townNamesList = Utility.readJsonFile(townNames)
+local Settings = Utility.readJsonFile(SettingsFile)
+if townNamesList and townNamesList.used and Settings then
+    townNamesList.used[Settings.town.name] = nil
+    Utility.writeJsonFile(townNames,townNamesList)
+end
 Utility.SelfDestruct()
