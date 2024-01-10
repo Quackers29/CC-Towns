7efaf1908bbfd3e03e969fa3898fc26bf4a62f3c
Gazer29 <gazer29@hotmail.com>
2024-01-10 00:46:47 +0000

    Clean up

--- Upgrades.lua
+++
@@ -1,5 +0,0 @@
-local upgrades = {
-logging_booster = {name = "Logging Booster", cost = {wood = 100, iron = 5}, alters = {trade_volume = 1.10}, locked_by = {"forest_production"}, duration = 5, maintenance_cost = "UNUSED", description = "Increases logging efficiency by 10%"}
-}
-
-return upgrades
