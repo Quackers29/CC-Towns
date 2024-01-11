f9b3ea9b969cd22dc874443138f40c43a6f36cc9
Gazer29 <gazer29@hotmail.com>
2024-01-11 19:44:18 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -111,16 +111,16 @@ if Settings and AdminSettings then
         Settings.general.biomeDist = dist or nil
     end
     if Settings.town.name == nil then
-        
+
         --Can be moved to Utility
         if McAPI.isAirBlock(x, y+1, z) then
             local facing = McAPI.GetFacing(x,y,z)
             local out = 2 -- blocks out from the centre of monitor
             local high = 3 --  block high of monitor
             if facing then
-                local x1,x2,z1,z2 = x,x,z+out,z-out
+                local x1,x2,z1,z2 = x,x,z-out,z+out
                 if facing == "north" or facing == "south" then
-                    x1,x2,z1,z2 = x+out,x-out,z,z
+                    x1,x2,z1,z2 = x-out,x+out,z,z
                 end
                 McAPI.fillArea(x1,y+1,z1,x2,y+high,z2, "computercraft:monitor_advanced[facing="..facing.."]{width:1}")
             end
