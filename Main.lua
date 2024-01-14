b7cb702dd122198d995f6f8e42f9eb1370e3e33c
Gazer29 <gazer29@hotmail.com>
2024-01-14 07:13:32 +0000

    Minor update

--- Main.lua
+++ Main.lua
@@ -81,6 +81,7 @@ Utility.CopyIfMissing(tradeSource,tradeFile)
 local Settings = Utility.readJsonFile(SettingsFile)
 
 if Settings and AdminSettings then
+    -- Biome search
     if Settings.general.biome == nil then
         local currentBiome = nil
         local dist = 9999
@@ -113,23 +114,15 @@ if Settings and AdminSettings then
         Settings.general.biome = currentBiome or nil
         Settings.general.biomeDist = dist or nil
     end
+
     if Settings.town.name == nil then
 
-        --Can be moved to Utility
-        
-        if McAPI.isAirBlock(x, y+1, z) then
-            local facing = McAPI.GetFacing(x,y,z)
-            local out = 2 -- blocks out from the centre of monitor
-            local high = 3 --  block high of monitor
-            if facing then
-                local x1,x2,z1,z2 = x,x,z-out,z+out
-                if facing == "north" or facing == "south" then
-                    x1,x2,z1,z2 = x-out,x+out,z,z
-                end
-                McAPI.fillArea(x1,y+1,z1,x2,y+high,z2, "computercraft:monitor_advanced[facing="..facing.."]{width:1}")
-            end
+        -- Builds the Monitor
+        if AdminSettings.generation.monitorBuild == true then
+            Utility.buildMonitor(x,y,z)
         end
 
+        -- Town name search
         local townNamesList = nil
         if not fs.exists(townNames) then
             local townNamesListSource = Utility.readJsonFile(townNamesSource)
